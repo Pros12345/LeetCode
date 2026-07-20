376907c8232baffb9d33b06c152dc49dd02b7281
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-07-21 00:32:28 +0530

    Time: 0 ms (100%), Space: 44.3 MB (57.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,15 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
-
-        if (list1.val <= list2.val) {
-            list1.next = mergeTwoLists(list1.next, list2);
-            return list1;
-        } else {
-            list2.next = mergeTwoLists(list1, list2.next);
-            return list2;
+        // 1. Create a dummy node to act as the head anchor
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+        
+        // 2. Iterate while both lists have remaining elements
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next; // Advance the tracking pointer
         }
+        
+        // 3. Connect the remaining nodes of the non-empty list
+        current.next = (list1 != null) ? list1 : list2;
+        
+        // 4. Return the head of the sorted merged list
+        return dummy.next;
     }
 }
