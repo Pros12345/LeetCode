f65a5fee84c8faa7b9d8655d614f10aa887cadf2
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-07-21 15:30:58 +0530

    Time: 4 ms (100%), Space: 46.1 MB (17.05%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,19 +1,31 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        // Negative numbers are not palindromes
+        if (x < 0)
+            return false;
 
-        String s = String.valueOf(x);
+        // Numbers ending with 0 (but not 0 itself) cannot be palindromes
+        if (x % 10 == 0 && x != 0)
+            return false;
 
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right))
-                return false;
-
-            left++;
-            right--;
+        int reversedHalf = 0;
+        // Reverse only half of the number
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
         }
 
-        return true;
+        // For even length numbers: x == reversedHalf
+        // For odd length numbers: x == reversedHalf/10
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+
+    public static void main(String[] args) {
+        Solution solver = new Solution();
+
+        System.out.println(solver.isPalindrome(121)); // true
+        System.out.println(solver.isPalindrome(-121)); // false
+        System.out.println(solver.isPalindrome(10)); // false
+        System.out.println(solver.isPalindrome(0)); // true
     }
 }
