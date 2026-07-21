1214ed1cb5569c7a644f1be5895dcde1b5708e81
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-07-21 16:59:26 +0530

    Time: 4 ms (97.25%), Space: 47.1 MB (64.68%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,36 +1,27 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
         int total = m * n;
 
-        k = k % total;
-
-        int[][] result = new int[m][n];
+        int[] arr = new int[total];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int index = i * n + j;
-                int newIndex = (index + k) % total;
-
-                int newRow = newIndex / n;
-                int newCol = newIndex % n;
-
-                result[newRow][newCol] = grid[i][j];
+                arr[(i * n + j + k) % total] = grid[i][j];
             }
         }
 
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int[] row : result) {
-            List<Integer> list = new ArrayList<>();
-            for (int val : row) {
-                list.add(val);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(arr[i * n + j]);
             }
-            ans.add(list);
+            result.add(row);
         }
 
-        return ans;
+        return result;
     }
 }
