a3563d4e8d2d1f162e693cd370dfad76ac1f4f9f
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-08-12 11:50:55 +0530

    Time: 3 ms (86.05%), Space: 43.6 MB (15.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,21 +2,15 @@ import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
-
         Stack<Character> stack = new Stack<>();
-
         for (char ch : s.toCharArray()) {
-
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
             } else {
-
                 if (stack.isEmpty()) {
                     return false;
                 }
-
                 char top = stack.pop();
-
                 if ((ch == ')' && top != '(') ||
                         (ch == '}' && top != '{') ||
                         (ch == ']' && top != '[')) {
@@ -24,7 +18,6 @@ class Solution {
                 }
             }
         }
-
         return stack.isEmpty();
     }
 }
