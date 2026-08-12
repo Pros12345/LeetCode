be059982b222b530273b72c3d88cde984684f344
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-08-12 11:48:32 +0530

    Time: 0 ms (100%), Space: 44.8 MB (48.63%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,10 +2,8 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int tar = 0;
         for (int i = 0; i < nums.length; i++) {
-
             if (target > nums[i]) {
                 tar = i + 1;
-
             }
             if (nums[i] == target) {
                 tar = i;
