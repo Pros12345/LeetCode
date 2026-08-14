9c0d91ac11fe23ef1b4a6a6e86a4d11d7b1b01c9
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-08-14 11:01:33 +0530

    Time: 12 ms (85.49%), Space: 47 MB (97.48%) - LeetHub

--- 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.java
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.java
@@ -4,31 +4,19 @@ class Solution {
     public int[][] minAbsDiff(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        
-        // Output dimensions match the number of valid sliding window positions
         int numRows = m - k + 1;
         int numCols = n - k + 1;
         int[][] result = new int[numRows][numCols];
-        
-        // Temporary array to hold elements of a single k x k submatrix
         int[] kgrid = new int[k * k];
-        
-        // Slide the submatrix window across the main grid
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
-                
-                // 1. Collect all elements inside the current k x k submatrix
                 int kIdx = 0;
                 for (int r = i; r < i + k; r++) {
                     for (int c = j; c < j + k; c++) {
                         kgrid[kIdx++] = grid[r][c];
                     }
                 }
-                
-                // 2. Sort the extracted submatrix elements
                 Arrays.sort(kgrid);
-                
-                // 3. Find the minimum absolute difference between adjacent distinct elements
                 int minDiff = Integer.MAX_VALUE;
                 for (int p = 1; p < kgrid.length; p++) {
                     if (kgrid[p] != kgrid[p - 1]) {
@@ -38,12 +26,9 @@ class Solution {
                         }
                     }
                 }
-                
-                // If all elements in the window are identical, the min unique difference is 0
                 result[i][j] = (minDiff == Integer.MAX_VALUE) ? 0 : minDiff;
             }
         }
-        
         return result;
     }
 }
