64db991d70a01a39800178e2ec00ab5e76797315
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-08-14 11:11:07 +0530

    Time: 0 ms (100%), Space: 47.4 MB (61.37%) - LeetHub

--- 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.java
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.java
@@ -2,11 +2,8 @@ class Solution {
     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
         int topRow = x;
         int bottomRow = x + k - 1;
-
-        // Perform vertical flip by swapping row segments
         while (topRow < bottomRow) {
             for (int col = y; col < y + k; col++) {
-                // Swap elements between topRow and bottomRow at the current column
                 int temp = grid[topRow][col];
                 grid[topRow][col] = grid[bottomRow][col];
                 grid[bottomRow][col] = temp;
@@ -14,7 +11,6 @@ class Solution {
             topRow++;
             bottomRow--;
         }
-
         return grid;
     }
 }
