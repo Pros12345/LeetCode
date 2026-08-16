c0f83d6d006a1561b0285815658134b74b08cabf
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-08-17 00:12:06 +0530

    Time: 7 ms (29.05%), Space: 114.6 MB (82.43%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -3,17 +3,16 @@ class Solution {
         int c0 = 0, c1 = 0, c2 = 0;
         for (int s : stones) {
             int r = s % 3;
-            if (r == 0) c0++;
-            else if (r == 1) c1++;
-            else c2++;
+            if (r == 0)
+                c0++;
+            else if (r == 1)
+                c1++;
+            else
+                c2++;
         }
-        
-        // Scenario 1: Even number of 0-remainder stones
         if (c0 % 2 == 0) {
             return c1 >= 1 && c2 >= 1;
         }
-        
-        // Scenario 2: Odd number of 0-remainder stones
         return Math.abs(c1 - c2) > 2;
     }
 }
