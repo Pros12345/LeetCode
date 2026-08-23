9f860acf1ab701f42ca4499052ef203638788050
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-08-23 10:11:16 +0530

    Time: 7 ms (79.56%), Space: 47.1 MB (41.61%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -3,16 +3,18 @@ class Solution {
         int n = num.length();
         int leftSum = 0, rightSum = 0;
         int leftQ = 0, rightQ = 0;
-
-        // Step 1: Count sums and '?' for both halves
         for (int i = 0; i < n; i++) {
             char c = num.charAt(i);
             if (i < n / 2) {
-                if (c == '?') leftQ++;
-                else leftSum += c - '0';
+                if (c == '?')
+                    leftQ++;
+                else
+                    leftSum += c - '0';
             } else {
-                if (c == '?') rightQ++;
-                else rightSum += c - '0';
+                if (c == '?')
+                    rightQ++;
+                else
+                    rightSum += c - '0';
             }
         }
         return 2 * (leftSum - rightSum) != 9 * (rightQ - leftQ);
