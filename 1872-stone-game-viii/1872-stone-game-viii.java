d53c7627ca857694185b8c9095742ee159594eec
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-08-24 10:05:23 +0530

    Time: 3 ms (100%), Space: 92.2 MB (37.02%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -6,13 +6,10 @@ class Solution {
         for (int i = 1; i < n; i++) {
             prefixSum[i] = prefixSum[i - 1] + stones[i];
         }
-
-        // res represents the max score difference from the current suffix max
         int res = prefixSum[n - 1];
         for (int i = n - 2; i >= 1; i--) {
             res = Math.max(res, prefixSum[i] - res);
         }
-
         return res;
     }
 }
