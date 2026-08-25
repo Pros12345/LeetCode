ddc74ca47e637cf57ccff94d4a129e928b0197f2
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-08-25 09:28:33 +0530

    Time: 2 ms (68.2%), Space: 45.7 MB (15.51%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -2,19 +2,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
-    // Renamed to match the judge's expected method name
     public int missingMultiple(int[] nums, int k) {
         Set<Integer> numSet = new HashSet<>();
         for (int num : nums) {
             numSet.add(num);
         }
-        
         int currentMultiple = k;
-        
         while (numSet.contains(currentMultiple)) {
             currentMultiple += k;
         }
-        
         return currentMultiple;
     }
 }
