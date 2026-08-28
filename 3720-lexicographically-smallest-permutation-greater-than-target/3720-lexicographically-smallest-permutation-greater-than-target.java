206dfc04cb0a6c2baf3f010de3a4ebe0301900fb
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-08-28 18:50:32 +0530

    Time: 2 ms (92.54%), Space: 44.8 MB (77.61%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
@@ -7,27 +7,27 @@ class Solution {
         for (int i = 0; i < n; i++) {
             count[s.charAt(i) - 'a']++;
         }
-        
+
         StringBuilder sb = new StringBuilder();
         if (dfs(0, true, count, sb, target, n)) {
             return sb.toString();
         }
         return "";
     }
-    
+
     private boolean dfs(int idx, boolean isPrefix, int[] count, StringBuilder sb, String target, int n) {
         if (idx == n) {
-            return !isPrefix; 
+            return !isPrefix;
         }
-        
+
         int start = isPrefix ? (target.charAt(idx) - 'a') : 0;
         for (int c = start; c < 26; c++) {
             if (count[c] > 0) {
                 count[c]--;
                 sb.append((char) ('a' + c));
-                
+
                 boolean nextPrefix = isPrefix && (c == start);
-                
+
                 if (!nextPrefix) {
                     fillRemaining(count, sb);
                     return true;
@@ -36,14 +36,14 @@ class Solution {
                         return true;
                     }
                 }
-                
+
                 sb.setLength(sb.length() - 1);
                 count[c]++;
             }
         }
         return false;
     }
-    
+
     private void fillRemaining(int[] count, StringBuilder sb) {
         for (int c = 0; c < 26; c++) {
             while (count[c] > 0) {
