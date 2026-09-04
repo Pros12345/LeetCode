e57a36c939fe5d662492deeff673915037613dc6
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-09-04 19:52:46 +0530

    Time: 1 ms (99.79%), Space: 46.3 MB (70.85%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -3,25 +3,25 @@ class Solution {
         int n = nums.length;
         int[] prefMax = new int[n];
         int[] suffMin = new int[n];
-        
+
         int maxVal = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
             maxVal = Math.max(maxVal, nums[i]);
             prefMax[i] = maxVal;
         }
-        
+
         int minVal = Integer.MAX_VALUE;
         for (int i = n - 1; i >= 0; i--) {
             minVal = Math.min(minVal, nums[i]);
             suffMin[i] = minVal;
         }
-        
+
         for (int i = 0; i < n; i++) {
             if ((long) prefMax[i] - suffMin[i] <= k) {
                 return i;
             }
         }
-        
+
         return -1;
     }
 }
