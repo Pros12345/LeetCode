5522adbb38059ae6e481e205cf92901500fdbeb3
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-09-04 19:53:46 +0530

    Time: 1 ms (99.79%), Space: 46.5 MB (15.74%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,27 +1,28 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-        int[] prefMax = new int[n];
         int[] suffMin = new int[n];
-
-        int maxVal = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            maxVal = Math.max(maxVal, nums[i]);
-            prefMax[i] = maxVal;
-        }
-
+        
+        // Pass 1: Backward pass to find suffix minimums
         int minVal = Integer.MAX_VALUE;
         for (int i = n - 1; i >= 0; i--) {
-            minVal = Math.min(minVal, nums[i]);
+            if (nums[i] < minVal) {
+                minVal = nums[i];
+            }
             suffMin[i] = minVal;
         }
-
+        
+        // Pass 2: Forward pass to compute prefix maximums and check stability on the fly
+        int maxVal = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
-            if ((long) prefMax[i] - suffMin[i] <= k) {
-                return i;
+            if (nums[i] > maxVal) {
+                maxVal = nums[i];
+            }
+            if ((long) maxVal - suffMin[i] <= k) {
+                return i; // Returns immediately at the very first stable index
             }
         }
-
+        
         return -1;
     }
 }
