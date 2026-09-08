355329653e757bea2699d464cf7fd74b00bb5b7b
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-09-08 10:14:04 +0530

    Time: 23 ms (27.59%), Space: 49.1 MB (8.22%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.java
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.java
@@ -1,15 +1,12 @@
 class Solution {
-
-    // Helper method to get coordinates of a character
     private int[] getCoord(char c) {
         int idx = c - 'A';
         return new int[] { idx / 6, idx % 6 };
     }
 
-    // Helper method to calculate Manhattan distance
     private int getDist(char a, char b) {
         if (a == 0)
-            return 0; // Free initial position
+            return 0;
         int[] c1 = getCoord(a);
         int[] c2 = getCoord(b);
         return Math.abs(c1[0] - c2[0]) + Math.abs(c1[1] - c2[1]);
@@ -32,13 +29,8 @@ class Solution {
         char curr = word.charAt(i);
         char charF1 = (f1 == 0) ? 0 : (char) ('A' + f1 - 1);
         char charF2 = (f2 == 0) ? 0 : (char) ('A' + f2 - 1);
-
-        // Option 1: Move finger 1 to curr
         int cost1 = getDist(charF1, curr) + dp(i + 1, curr - 'A' + 1, f2, word, memo);
-
-        // Option 2: Move finger 2 to curr
         int cost2 = getDist(charF2, curr) + dp(i + 1, f1, curr - 'A' + 1, word, memo);
-
         return memo[i][f1][f2] = Math.min(cost1, cost2);
     }
 }
