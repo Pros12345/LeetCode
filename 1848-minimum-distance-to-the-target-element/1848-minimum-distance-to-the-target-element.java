5601fe875df6a0e2e344718469838f9b7976a48b
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-09-08 11:13:40 +0530

    Time: 1 ms (67.94%), Space: 45.1 MB (56%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
@@ -1,19 +1,15 @@
 class Solution {
     public int getMinDistance(int[] nums, int target, int start) {
         int minDistance = Integer.MAX_VALUE;
-
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target) {
                 int currentDistance = Math.abs(i - start);
                 minDistance = Math.min(minDistance, currentDistance);
-
-                // Optimization: 0 is the minimum possible distance
                 if (minDistance == 0) {
                     break;
                 }
             }
         }
-
         return minDistance;
     }
 }
