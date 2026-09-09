4ec2836caa86ba5c70864e2960d8166492514ae2
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-09-09 10:04:21 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (37.18%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -3,16 +3,16 @@ class Solution {
         long totalCommas = 0;
         long threshold = 1000;
         long commasPerNumber = 1;
-        
+
         while (threshold <= n) {
             long nextThreshold = threshold * 1000;
             long count = Math.min(n, nextThreshold - 1) - threshold + 1;
             totalCommas += count * commasPerNumber;
-            
+
             threshold = nextThreshold;
             commasPerNumber++;
         }
-        
+
         return totalCommas;
     }
 }
