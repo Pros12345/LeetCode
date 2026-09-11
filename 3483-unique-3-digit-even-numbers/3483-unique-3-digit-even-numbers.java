6076343e8ced317b9a219d4fe9af360bf00ffb51
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-09-11 10:29:19 +0530

    Time: 11 ms (19.47%), Space: 46.9 MB (15.2%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -4,19 +4,19 @@ class Solution {
         for (int d : digits) {
             count[d]++;
         }
-        
+
         int distinctEvenCount = 0;
-        
+
         // Iterate through all 3-digit even numbers
         for (int num = 100; num < 1000; num += 2) {
             if (isValid(num, count)) {
                 distinctEvenCount++;
             }
         }
-        
+
         return distinctEvenCount;
     }
-    
+
     private boolean isValid(int num, int[] availableCount) {
         int[] numCount = new int[10];
         while (num > 0) {
@@ -24,14 +24,14 @@ class Solution {
             numCount[digit]++;
             num /= 10;
         }
-        
+
         // Check if we have enough copies of each digit
         for (int i = 0; i < 10; i++) {
             if (numCount[i] > availableCount[i]) {
                 return false;
             }
         }
-        
+
         return true;
     }
 }
