a6ea63b6aed3ae94a3fa266a8976ac9722552af7
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-09-16 10:42:28 +0530

    Time: 16 ms (85.81%), Space: 42 MB (96.13%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -2,23 +2,17 @@ class Solution {
     public int numberOfSets(int n, int k) {
         int totalN = n + k - 1;
         int totalK = 2 * k;
-
-        // If it's impossible to pick 2k points out of totalN, return 0
         if (totalK > totalN) {
             return 0;
         }
-
         int MOD = 1_000_000_007;
         int[] dp = new int[totalK + 1];
-        dp[0] = 1; // Base case: 1 way to choose 0 items
-
-        // Iteratively build Pascal's triangle up to totalK
+        dp[0] = 1;
         for (int i = 1; i <= totalN; i++) {
             for (int j = Math.min(i, totalK); j > 0; j--) {
                 dp[j] = (dp[j] + dp[j - 1]) % MOD;
             }
         }
-
         return dp[totalK];
     }
 }
