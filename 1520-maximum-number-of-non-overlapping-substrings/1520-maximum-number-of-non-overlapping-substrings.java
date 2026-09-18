01a65be6a6a7833d21c1dc960599431a065a3e1e
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-09-18 10:00:38 +0530

    Time: 9 ms (62.92%), Space: 47.9 MB (78.26%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
@@ -6,25 +6,26 @@ class Solution {
         int n = s.length();
         int[] left = new int[26];
         int[] right = new int[26];
-        
+
         for (int i = 0; i < 26; i++) {
             left[i] = n;
             right[i] = -1;
         }
-        
+
         for (int i = 0; i < n; i++) {
             int idx = s.charAt(i) - 'a';
             left[idx] = Math.min(left[idx], i);
             right[idx] = i;
         }
-        
+
         List<String> ans = new ArrayList<>();
         int lastRight = -1;
-        
+
         for (int i = 0; i < n; i++) {
             int idx = s.charAt(i) - 'a';
-            if (i != left[idx]) continue;
-            
+            if (i != left[idx])
+                continue;
+
             int newRight = getValidEnd(s, i, left, right);
             if (newRight != -1) {
                 if (i <= lastRight && !ans.isEmpty()) {
@@ -35,10 +36,10 @@ class Solution {
                 lastRight = newRight;
             }
         }
-        
+
         return ans;
     }
-    
+
     private int getValidEnd(String s, int start, int[] left, int[] right) {
         int end = right[s.charAt(start) - 'a'];
         for (int j = start; j <= end; j++) {
