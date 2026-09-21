2fe32e30063d386c115863867ea2869d8701c768
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-09-21 15:34:13 +0530

    Time: 1 ms (100%), Space: 44.1 MB (34.14%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -5,13 +5,13 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
-            
+
             // Calculate reversed alphabet position ('a' = 26, 'b' = 25, ..., 'z' = 1)
             int reversedAlphabetPos = 26 - (c - 'a');
-            
+
             // 1-indexed position in the string
             int stringPos = i + 1;
-            
+
             // Add the product to the cumulative sum
             totalDegree += reversedAlphabetPos * stringPos;
         }
