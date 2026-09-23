d7dc5f63bc50b6efd24601e4c427eb08ae5e5e00
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-09-23 10:24:49 +0530

    Time: 4 ms (98.07%), Space: 102.3 MB (29.35%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -4,7 +4,7 @@ class Solution {
         for (int num : nums) {
             totalSum += num;
         }
-        
+
         int target = totalSum - x;
         if (target < 0) {
             return -1;
@@ -12,24 +12,24 @@ class Solution {
         if (target == 0) {
             return nums.length;
         }
-        
+
         int maxLen = -1;
         int currentSum = 0;
         int left = 0;
-        
+
         for (int right = 0; right < nums.length; right++) {
             currentSum += nums[right];
-            
+
             while (currentSum > target && left <= right) {
                 currentSum -= nums[left];
                 left++;
             }
-            
+
             if (currentSum == target) {
                 maxLen = Math.max(maxLen, right - left + 1);
             }
         }
-        
+
         return maxLen == -1 ? -1 : nums.length - maxLen;
     }
 }
