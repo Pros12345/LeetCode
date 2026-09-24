8ceb3dd2fa2a8314f45df3ef72706528b9b820ac
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-09-24 10:38:12 +0530

    Time: 1 ms (99.69%), Space: 45.6 MB (59.67%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,16 +1,13 @@
 class Solution {
     public int smallestIndex(int[] nums) {
-        // Iterate sequentially through the array to find the smallest index first
         for (int i = 0; i < nums.length; i++) {
             if (getDigitSum(nums[i]) == i) {
                 return i;
             }
         }
-        // Return -1 if no such index satisfies the condition
         return -1;
     }
 
-    // Helper method to calculate the sum of digits of a given number
     private int getDigitSum(int num) {
         int sum = 0;
         while (num > 0) {
